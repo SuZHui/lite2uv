a3e24c63d7ca49aa89415842fa82bacc692dbe11
suzh <szh362680581@gmail.com>
2022-04-02 17:52:34 +0800

    update: reactive组件追加单元测试

--- packages/reactivity/__tests__/effect.spec.ts
+++ packages/reactivity/__tests__/effect.spec.ts
@@ -1,9 +1,21 @@
-import { effect } from '../src/index'
+import { effect, reactive } from '../src/index'
 
 describe('reactivity/effect', () => {
+  // `effect`会执行一次传入的函数
   it('should run the passed function once (wrapped by a effect)', () => {
     const fnSpy = jest.fn()
-    effect()
+    effect(fnSpy)
     expect(fnSpy).toHaveBeenCalledTimes(1)
+  })
+
+  // 可监听对象基础属性的变化
+  it.skip('should observe basic properties', () => {
+    // let dummy
+    // const counter = reactive({ num: 0 })
+    // effect(() => (dummy = counter.num))
+
+    // expect(dummy).toBe(0)
+    // counter.num = 7
+    // expect(dummy).toBe(7)
   })
 })
--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,3 +1,14 @@
-export function effect() {
-  
+export class ReactiveEffect<T = any> {
+  constructor(
+    public fn: () => T,
+  ) {}
+
+  run() {
+    return this.fn()
+  }
+}
+
+export function effect<T = any>(fn: () => T) {
+  const _effect = new ReactiveEffect(fn)
+  _effect.run()
 }
