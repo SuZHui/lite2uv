27f8f0430f5bedb76f5a25b8e3f341d92e42a917
suzh <szh362680581@gmail.com>
2022-07-08 14:34:11 +0800

    update(readonly): readonly object类型增加单测

--- packages/global.d.ts
+++ packages/global.d.ts
@@ -1,2 +1,11 @@
 // Global compile-time constants
 declare var __DEV__: boolean
+
+// for tests
+declare namespace jest {
+  interface Matchers<R, T> {
+    toHaveBeenWarned(): R
+    toHaveBeenWarnedLast(): R
+    toHaveBeenWarnedTimes(n: number): R
+  }
+}
--- packages/reactivity/__tests__/readonly.spec.ts
+++ packages/reactivity/__tests__/readonly.spec.ts
@@ -1,5 +1,9 @@
-import { isReactive, readonly, isProxy, isReadonly } from "../src"
+import { isReactive, readonly, isProxy, isReadonly, effect } from "../src"
 
+/**
+ * @see https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-4.html
+ */
+type Writable<T> = { -readonly [P in keyof T]: T[P] }
 
 describe('reactivity/readonly', () => {
   describe('Object', () => {
@@ -22,6 +26,70 @@ describe('reactivity/readonly', () => {
       expect('foo' in wrapped).toBe(true)
       // ownKeys
       expect(Object.keys(wrapped)).toEqual(['foo', 'bar'])
+    })
+
+    it('should not allow mutation', () => {
+      const qux = Symbol('qux')
+      const original = {
+        foo: 1,
+        bar: {
+          baz: 2
+        },
+        [qux]: 3
+      }
+      const wrapped: Writable<typeof original> = readonly(original)
+
+      wrapped.foo = 2
+      expect(wrapped.foo).toBe(1)
+      expect(
+        `Set operation on key "foo" failed: target is readonly.`
+      ).toHaveBeenWarnedLast()
+
+      wrapped.bar.baz = 3
+      expect(wrapped.bar.baz).toBe(2)
+      expect(
+        `Set operation on key "baz" failed: target is readonly.`
+      ).toHaveBeenWarnedLast()
+
+      wrapped[qux] = 4
+      expect(wrapped[qux]).toBe(3)
+      expect(
+        `Set operation on key "Symbol(qux)" failed: target is readonly.`
+      ).toHaveBeenWarnedLast()
+
+      // @ts-expect-error
+      delete wrapped.foo
+      expect(wrapped.foo).toBe(1)
+      expect(
+        `Delete operation on key "foo" failed: target is readonly.`
+      ).toHaveBeenWarnedLast()
+
+      // @ts-expect-error
+      delete wrapped.bar.baz
+      expect(wrapped.bar.baz).toBe(2)
+      expect(
+        `Delete operation on key "baz" failed: target is readonly.`
+      ).toHaveBeenWarnedLast()
+
+      // @ts-expect-error
+      delete wrapped[qux]
+      expect(wrapped[qux]).toBe(3)
+      expect(
+        `Delete operation on key "Symbol(qux)" failed: target is readonly.`
+      ).toHaveBeenWarnedLast()
+    })
+
+    it('should not trigger effects', () => {
+      const wrapped: any = readonly({ a: 1 })
+      let dummy
+      effect(() => {
+        dummy = wrapped.a
+      })
+      expect(dummy).toBe(1)
+      wrapped.a = 2
+      expect(wrapped.a).toBe(1)
+      expect(dummy).toBe(1)
+      expect(`target is readonly`).toHaveBeenWarned()
     })
   })
 })
