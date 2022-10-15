f76716dde806eb5acdb11beabf6e620f6a35e149
suzh <szh362680581@gmail.com>
2022-10-15 15:47:50 +0800

    update(computed): add computed unit test

--- packages/reactivity/src/index.ts
+++ packages/reactivity/src/index.ts
@@ -12,4 +12,4 @@ export {
 export { reactive, readonly, shallowReactive, isShallow, isReadonly, isReactive, isProxy, markRaw, toRaw } from './reactive'
 export { effect, stop, ReactiveEffectRunner, DebuggerEvent, ITERATE_KEY } from './effect'
 export { TrackOpTypes, TriggerOpTypes } from './operations'
-export { computed } from './computed'
+export { computed, WritableComputedRef } from './computed'
